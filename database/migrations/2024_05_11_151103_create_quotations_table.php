0a5a1167b199c7595e99e0633a2fdaf7b168697e
Rameez <121714501+rameezmuhammadh@users.noreply.github.com>
2024-05-13 00:21:03 +0530

    feat: Create migrations for quotations table

---
+++ database/migrations/2024_05_11_151103_create_quotations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('quotations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('prescription_id')->constrained()->cascadeOnDelete();
+            $table->json('drugs');
+            $table->enum('status', ['accept', 'reject'])->nullable();
+            $table->string('total_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('quotations');
+    }
+};
