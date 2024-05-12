471e00cca7d2ba09a2ff077a7da0a9b9eee0f340
Rameez <121714501+rameezmuhammadh@users.noreply.github.com>
2024-05-13 00:22:23 +0530

    feat: Add routes for prescription and quotation resources

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\Admin\DrugController as AdminDrugController;
+use App\Http\Controllers\Admin\PrescriptionController as AdminPrescriptionController;
+use App\Http\Controllers\Admin\QuotationController as AdminQuotationController;
+use App\Http\Controllers\PrescriptionController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\QuotationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,5 +21,17 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('/prescription',PrescriptionController::class);
+Route::resource('/quotation',QuotationController::class);
+
+
+Route::middleware('auth')->name('admin.')->prefix('admin')->group(function () {
+    Route::resource('drug',AdminDrugController::class);
+    Route::resource('prescription',AdminPrescriptionController::class);
+    Route::resource('quotation',AdminQuotationController::class);
+    
+});
+
 
 require __DIR__.'/auth.php';
