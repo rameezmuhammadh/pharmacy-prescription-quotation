3a058d07df6b828d540c86722ccb4a06d073bcbe
Rameez <121714501+rameezmuhammadh@users.noreply.github.com>
2024-05-14 22:35:04 +0530

    feat: Update Quotation Show page layout
    The Quotation Show page layout in the `Show.blade.php` file has been updated. Changes include removing unnecessary table columns and improving whitespace and alignment. This improves the readability and presentation of the Quotation Show page.

--- resources/views/admin/Prescription/Show.blade.php
+++ resources/views/admin/Prescription/Show.blade.php
@@ -43,8 +43,7 @@
                                             <th
                                                 class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
                                                 Sub total </th>
-                                            <th>
-                                            </th>
+                                         
                                         </tr>
                                     </thead>
                                     <tbody>
@@ -63,9 +62,7 @@
                                                 {{ $quotation->total_price }}
 
                                             </td>
-                                            <td
-                                                class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700">
-                                                Del</td>
+                                            
                                         </tr>
                                         @endforeach
                                         <!-- Add this code at the top of your Blade file -->
