87e92766d848f07a5af54cc5916da7ac3e46471e
Rameez <121714501+rameezmuhammadh@users.noreply.github.com>
2024-05-14 07:02:41 +0530

    feat: Update "Show" button on Prescription Show page

--- resources/views/admin/Prescription/Show.blade.php
+++ resources/views/admin/Prescription/Show.blade.php
@@ -108,11 +108,14 @@
                             </div>
                             <div class="flex items-center justify-end mt-4">
                                 <hr class="h-2 bg-slate-500">
-                                <a class="ms-4 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
-                                href="{{ route('admin.quotation.send') }}"
-                                >
-                                    Send Quotation
-                                </a>
+                                <form action="{{ route('admin.quotation.send') }}" method="POST">
+                                    @csrf
+                                    <input type="hidden" value="{{ $prescription->id }}" name="prescription_id">
+                                    <x-primary-button class="ms-4">
+
+                                        {{ __('Send Quations') }}
+                                    </x-primary-button>
+                            </form>
                             </div>
                         </div>
                     </div>
