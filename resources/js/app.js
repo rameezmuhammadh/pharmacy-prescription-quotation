e2296d25206fbc39f25ba998f13d481de218ad05
Rameez <121714501+rameezmuhammadh@users.noreply.github.com>
2024-05-14 07:21:07 +0530

    feat: Add lazy loading for product images and improve product gallery functionality

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,17 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+function imagePointer(smallImg){
+    let fullImg = document.getElementById("product-img");
+    fullImg.src=smallImg.src;
+ }
+
+// js for prouduct gallery 
+
+var ProductImg = document.getElementById(product-img);
+var smallImg = document.getElementById("small-img");
+
+smallImg[0].onclick = function(){
+   ProductImg.src=smallImg[0].src;
+}
