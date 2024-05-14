10ac7f7513260e30da1b443075964f9d565530da
Rameez <121714501+rameezmuhammadh@users.noreply.github.com>
2024-05-14 22:07:35 +0530

    feat: Update routes to include middleware for authentication and verification
    This commit updates the routes in the web.php file to include middleware for authentication and verification. The '/prescription' and '/quotation' routes now require the user to be authenticated and verified before accessing them. Additionally, the '/admin' routes now only require authentication. This change enhances the security and access control of the application.

--- routes/web.php
+++ routes/web.php
@@ -20,18 +20,17 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::resource('/prescription',PrescriptionController::class);
-    Route::resource('/quotation',QuotationController::class);
 });
 
+Route::resource('/prescription', PrescriptionController::class)->middleware(['auth', 'verified']);
+Route::resource('/quotation', QuotationController::class)->middleware(['auth', 'verified']);
 
-
-Route::middleware(['auth', 'role:admin'])->name('admin.')->prefix('admin')->group(function () {
-    Route::resource('drug',AdminDrugController::class);
-    Route::resource('prescription',AdminPrescriptionController::class);
-    Route::resource('quotation',AdminQuotationController::class);
+Route::middleware(['auth'])->name('admin.')->prefix('admin')->group(function () {
+    Route::resource('drug', AdminDrugController::class);
+    Route::resource('prescription', AdminPrescriptionController::class);
+    Route::resource('quotation', AdminQuotationController::class);
     Route::post('/send-quotation', [AdminQuotationController::class, 'sendQuotation'])->name('quotation.send');
 });
 
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
